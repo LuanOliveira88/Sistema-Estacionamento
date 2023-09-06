c7dcf311bcdcccafd3766feeb282bfccd49179b5
luan-oliveira <olivrluan@gmail.com>
2023-09-06 14:15:07 -0300

    atualizando layout

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     page.theme_mode = ft.ThemeMode.LIGHT
+    page.window_maximized = True
 
     def inserir_registro(e):
         if carro.value:
@@ -28,20 +29,54 @@ def main(page: ft.Page):
                     # _tabela.build_update_commands()
                     page.update()
 
-                _tabela.rows.append(ft.DataRow(cells=[
-                    ft.DataCell(ft.Text(value=(carro_valor := carro.value))),
-                    ft.DataCell(ft.Text(value=(placa_valor := placa.value))),
-                    ft.DataCell(ft.Text(f"{(t_entrada := datetime.now().strftime(r'%H:%M:%S'))}")),
-                    ft.DataCell(ft.Text(value=(t_saida := "Aguardando saída"), italic=True)),
-                    ft.DataCell(ft.Text(value=(valor_pagar := "Aguardando saída"), italic=True)),
-                    ft.DataCell(
-                        content=(
-                            b := ft.ElevatedButton(
-                                width=200,
-                                text='Encerrar',
-                                on_click=encerrar_registro,
-                                data=[carro_valor, placa_valor, t_entrada, t_saida, valor_pagar])))
-                ]))
+                _tabela.rows.append(
+                    ft.DataRow(
+                        cells=[
+                            ft.DataCell(
+                                ft.Text(
+                                    value=(carro_valor := carro.value)
+                                )
+                            ),
+                            ft.DataCell(
+                                ft.Text(
+                                    value=(placa_valor := placa.value)
+                                )
+                            ),
+                            ft.DataCell(
+                                ft.Text(
+                                    value=f"{(t_entrada := datetime.now().strftime(r'%H:%M:%S'))}")
+                            ),
+                            ft.DataCell(
+                                ft.Text(
+                                    value=(t_saida := "Aguardando saída"),
+                                    italic=True
+                                )
+                            ),
+                            ft.DataCell(
+                                ft.Text(
+                                    value=(valor_pagar := "Aguardando saída"),
+                                    italic=True
+                                )
+                            ),
+                            ft.DataCell(
+                                content=(
+                                    b := ft.ElevatedButton(
+                                        width=110,
+                                        text='Encerrar',
+                                        on_click=encerrar_registro,
+                                        data=[
+                                            carro_valor,
+                                            placa_valor,
+                                            t_entrada,
+                                            t_saida,
+                                            valor_pagar
+                                        ]
+                                    )
+                                )
+                            )
+                        ]
+                    )
+                )
                 carro.value = ''
                 placa.value = ''
                 page.update()
@@ -54,7 +89,7 @@ def main(page: ft.Page):
                 page.update()
 
     carro = ft.Dropdown(
-        width=800,
+        width=(wdth := 400),
         label='Carro',
         autofocus=True,
         hint_text='Escolha uma das opções',
@@ -70,33 +105,48 @@ def main(page: ft.Page):
         ])
 
     placa = ft.TextField(
+        width=wdth,
         max_length=7,
         label='Placa',
         autofocus=True,
         # helper_text='Digite a placa no carro no formato XXXXXXX',
         # border=ft.InputBorder.UNDERLINE,
         text_size=20,
-        width=300,
         expand=False,
         capitalization=ft.TextCapitalization.CHARACTERS
     )
 
-    container_registros = ft.Column(controls=[carro, placa])
-    page.add(
-        container_registros,
-        ft.ElevatedButton(width=150,
-                          content=ft.Row(
-                              controls=[
-                                  ft.Icon(ft.icons.ADD),
-                                  ft.Text("Adicionar")
-                              ]),
-                          on_click=inserir_registro)
+    add_btn = ft.ElevatedButton(
+        width=wdth,
+        content=ft.Row(
+            controls=[
+                ft.Icon(
+                    ft.icons.ADD
+                ),
+                ft.Text(
+                    text_align=ft.TextAlign.CENTER,
+                    value="Adicionar"
+                )
+            ]
+        ),
+        on_click=inserir_registro
     )
 
     page.add(
-        (_tabela := ft.DataTable(
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[carro]),
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[placa]),
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[add_btn]
+        )
+    )
+    _tabela = ft.DataTable(
             width=800,
-            bgcolor="blue",
+            bgcolor="YELLOW",
             horizontal_lines=ft.border.BorderSide(0.5, "Gray"),
             vertical_lines=ft.border.BorderSide(0.5, "Gray"),
             columns=[
@@ -105,10 +155,21 @@ def main(page: ft.Page):
                 ft.DataColumn(ft.Text("Entrada")),
                 ft.DataColumn(ft.Text("Saída")),
                 ft.DataColumn(ft.Text("Total a Pagar")),
-                ft.DataColumn(ft.Text(""))
+                ft.DataColumn(
+                    ft.Text(
+                        text_align=ft.TextAlign.CENTER,
+                        width=100,
+                        value="Check"
+                    )
+                )
             ],
         )
-         ))
+    page.controls.append(
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[_tabela]
+        )
+    )
 
     page.scroll = 'always'
     page.update()
